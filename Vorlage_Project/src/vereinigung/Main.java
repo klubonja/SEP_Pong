cd052691760f712c7ddacb08e1a87e190252142e
lubonja <lubonja@cip.ifi.lmu.de>
2015-05-08 20:38:49 +0200

    Final Main with edited bugs.

--- Vorlage_Project/src/vereinigung/Main.java
+++ Vorlage_Project/src/vereinigung/Main.java
@@ -5,7 +5,10 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,32 +18,28 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.effect.BlurType;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Glow;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
-	final Image gestreifteRemulanerHintergrund1 = new Image("/Hintergrund1.png");
+	
 
 	SimpleStringProperty footerError = new SimpleStringProperty("");
 	boolean players;
-
-	Controller selectedGameStart;
+	Controller newGame;
 
 	public void start(Stage stage) throws Exception {
-		ImageView background1 = new ImageView();
-		background1.setImage(gestreifteRemulanerHintergrund1);
 
-		Group root = new Group();
-		Scene scene = new Scene(root, 1000, 600);
 		GridPane gp = new GridPane();
+		Scene scene = new Scene(gp, 1000, 650);
+		gp.setAlignment(Pos.CENTER);
 		gp.setVgap(20);
 		gp.setHgap(35);
+		gp.setPadding(new Insets(20));
 		gp.getRowConstraints().add(new RowConstraints(150));
 		gp.getRowConstraints().add(new RowConstraints(50));
 		gp.getRowConstraints().add(new RowConstraints(50));
@@ -49,18 +48,19 @@ public class Main extends Application {
 		gp.getRowConstraints().add(new RowConstraints(50));
 		gp.setMinHeight(600);
 		gp.setMinWidth(1000);
+		gp.setStyle("-fx-background-color: radial-gradient( radius 100%, lightgreen, lightseagreen)");
 
 		DropShadow dropShadow = new DropShadow();
-		dropShadow.setRadius(8.0);
+		dropShadow.setRadius(1.0);
 		dropShadow.setBlurType(BlurType.GAUSSIAN);
 
 		// ueberschrift
 		Label title = new Label("Ping-Pong");
-		title.setEffect(new Glow());
+		title.setEffect(new Glow(0.7));
 		title.setStyle("-fx-font-size: 80");
 		// untertitel
 		Label author = new Label("Gestreifte Remulaner 2015");
-		author.setEffect(new Glow());
+		author.setEffect(new Glow(0.5));
 		author.setStyle("-fx-font-size: 38");
 		// label fuer die auswahl von spielern
 		Label selectPlayer = new Label("Select number of PLAYERS");
@@ -81,7 +81,7 @@ public class Main extends Application {
 		playerNumber1.setToggleGroup(group);
 		// zweiter radio button
 		RadioButton playerNumber2 = new RadioButton();
-		playerNumber2.setText("2 Player");
+		playerNumber2.setText("2 Players");
 		playerNumber2.setStyle("-fx-font-size: 30");
 		playerNumber2.setEffect(dropShadow);
 		playerNumber2.setToggleGroup(group);
@@ -96,7 +96,7 @@ public class Main extends Application {
 		TextField ballNumber = new TextField();
 		ballNumber.setEffect(dropShadow);
 		ballNumber.setPromptText("e.g. 5");
-		ballNumber.setPrefWidth(30);
+		ballNumber.setPrefWidth(50);
 		// button play
 		Button startGame = new Button("Play");
 		startGame.setEffect(dropShadow);
@@ -114,23 +114,29 @@ public class Main extends Application {
 		});
 		// fehlermeldung
 		Label error = new Label("");
-		error.setStyle("-fx-font-size: 500");
+		error.setStyle("-fx-font-size: 25");
+		error.setTextFill(Color.RED);
 		error.textProperty().bind(footerError);
 
 		GridPane.setConstraints(title, 0, 0);
+		GridPane.setColumnSpan(title, 3);
+		GridPane.setHalignment(title, HPos.CENTER);
+		GridPane.setValignment(title, VPos.BOTTOM);
 		GridPane.setConstraints(author, 0, 1);
+		GridPane.setColumnSpan(author, 3);
+		GridPane.setHalignment(author, HPos.CENTER);
+		GridPane.setValignment(author, VPos.TOP);
 		GridPane.setConstraints(selectPlayer, 0, 2);
-		//GridPane.setColumnSpan(selectPlayer, 2);
 		GridPane.setConstraints(playerNumber, 1, 2);
-		//GridPane.setColumnSpan(playerNumber, 2);
 		GridPane.setConstraints(selectBalls, 0, 3);
-		GridPane.setColumnSpan(selectBalls, 2);
 		GridPane.setConstraints(ballNumber, 1, 3);
-		GridPane.setColumnSpan(ballNumber, 2);
 		GridPane.setConstraints(startGame, 0, 4);
-		GridPane.setConstraints(quitGame, 2, 4);
-		GridPane.setConstraints(error, 1, 5);
-		gp.getChildren().addAll(background1, title, author, selectPlayer,
+		GridPane.setConstraints(quitGame, 1, 4);
+		GridPane.setHalignment(quitGame, HPos.RIGHT);
+		GridPane.setConstraints(error, 0, 5);
+		GridPane.setColumnSpan(error, 3);
+		GridPane.setHalignment(error, HPos.CENTER);
+		gp.getChildren().addAll(title, author, selectPlayer,
 				playerNumber, selectBalls, ballNumber, startGame, quitGame,
 				error);
 
@@ -138,30 +144,45 @@ public class Main extends Application {
 			public void handle(MouseEvent e) {
 				if (playerNumber1.isSelected()) {
 					players = true;
-				} else if (!playerNumber1.isSelected()
-						&& !playerNumber2.isSelected()) {
-					footerError.set("Please select only one option.");
-				} else if (playerNumber2.isSelected()) {
-					players = false;
-				}
-
-				int numberOfBalls;
-				try {
-					numberOfBalls = Integer.parseInt(ballNumber.getText());
-					if (numberOfBalls > 0){
-						selectedGameStart = new Controller(players,
-								numberOfBalls);
-						stage.close();
+					
+					int numberOfBalls;
+					try {
+						numberOfBalls = Integer.parseInt(ballNumber.getText());
+						if (numberOfBalls > 0){
+							newGame = new Controller(players,
+									numberOfBalls);
+							stage.close();
+						}
+					}catch (IllegalArgumentException e1) {
+						footerError.setValue("Please select the number of balls!");
 					}
-				} catch (IllegalArgumentException e1) {
-					footerError.setValue("Please select number of balls!");
+					
+				}else if (playerNumber2.isSelected()) {
+					players = false;
+					
+					int numberOfBalls;
+					try {
+						numberOfBalls = Integer.parseInt(ballNumber.getText());
+						if (numberOfBalls > 0){
+							newGame = new Controller(players,
+									numberOfBalls);
+							stage.close();
+						}
+					}catch (IllegalArgumentException e1) {
+						footerError.setValue("Please select the number of balls!");
+					}
+					
+				}else if (!playerNumber1.isSelected()
+						&& !playerNumber2.isSelected()) {
+					footerError.set("Please select a playing mode!");
 				}
 			}
 		});
 		
-		root.getChildren().addAll(background1, gp);
+		stage.setResizable(false);
+		//stage.setTitle("Pong SEP2015");
 		stage.setScene(scene);
-		stage.initStyle(StageStyle.UNDECORATED);
+		//stage.initStyle(StageStyle.UNDECORATED);
 		stage.show();
 	}
 
